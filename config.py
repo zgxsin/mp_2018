ea5314f83bb58f985a7587b9cade3c4e1cbcd425
Hari <g.x.zhou@outlook.com>
2018-06-07 19:17:50 +0200

    No L1 regularizer, 2 LSTM cells, 8/2

--- config.py
+++ config.py
@@ -10,13 +10,13 @@ config['log_dir'] = './runs/'
 # In case your pre/post-processing scripts generate intermediate results, you may use config['tmp_dir'] to store them.
 config['tmp_dir'] = './tmp/'
 # Path to training, validation and test data folders.
-config['train_data_dir'] = "../train/"
-config['valid_data_dir'] = "../validation/"
-config['test_data_dir'] = "../test/"
+# config['train_data_dir'] = "../train/"
+# config['valid_data_dir'] = "../validation/"
+# config['test_data_dir'] = "../test/"
 
-# config['train_data_dir'] = "/cluster/work/riner/users/zgxsin/mp2018/dataset/train"
-# config['valid_data_dir'] = "/cluster/work/riner/users/zgxsin/mp2018/dataset/validation/"
-# config['test_data_dir'] = "/cluster/work/riner/users/zgxsin/mp2018/dataset/test/"
+config['train_data_dir'] = "/cluster/work/riner/users/zgxsin/mp2018/dataset/train"
+config['valid_data_dir'] = "/cluster/work/riner/users/zgxsin/mp2018/dataset/validation/"
+config['test_data_dir'] = "/cluster/work/riner/users/zgxsin/mp2018/dataset/test/"
 ##################################################################
 # You can modify the rest or add new fields as you need.
 
@@ -39,7 +39,7 @@ config['num_epochs'] = 25
 config['evaluate_every_step'] = config['num_steps_per_epoch']*2 # every two epoch, evaluate the model performance
 config['checkpoint_every_step'] = config['num_steps_per_epoch']*5 # every 5 epoch save the model
 config['num_validation_steps'] = int(config['num_validation_samples']/config['batch_size'])
-config['print_every_step'] = 2
+config['print_every_step'] = 50
 
 # Here I provide three common techniques to calculate sequence loss.
 # (1) 'last_logit': calculate loss by using only the last step prediction.
@@ -51,7 +51,7 @@ config['loss_type'] = 'average_logit' # 'last_logit', 'average_logit', 'average_
 # Dataset and Input Pipeline Configuration
 config['inputs'] = {}
 config['inputs']['queue_capacity'] = config['batch_size']*20
-config['inputs']['num_read_threads'] = 4
+config['inputs']['num_read_threads'] = 10
 config['inputs']['num_epochs'] = config['num_epochs']
 config['inputs']['batch_size'] = config['batch_size']
 config['inputs']['img_height'] = 80  #Input size.
@@ -61,7 +61,6 @@ config['inputs']['skeleton_size'] = 180
 ## setting the 3dcnn frame lenth and overlap
 config['frame_lenth'] = 8
 config['real_frame_overlap'] = 2
-
 config['frame_overlap'] = config['frame_lenth'] - config['real_frame_overlap']
 
 
@@ -76,7 +75,7 @@ config['cnn']['dropout_rate'] = 0.3
 config['cnn']['num_class_labels'] = 20
 config['cnn']['batch_size'] = config['batch_size']
 config['cnn']['loss_type'] = config['loss_type']
-config['cnn']['regularization_rate'] = 0.0001 # this is the rate for L2 or L1 regularizer
+config['cnn']['regularization_rate'] = 0 # this is the rate for L2 or L1 regularizer
 config['cnn']['moving_average_decay'] = 0.998 # moving_average_rate
 config['cnn']['frame_lenth'] = config['frame_lenth']
 config['cnn']['frame_overlap'] = config['frame_overlap']
@@ -88,11 +87,11 @@ config['rnn']['num_hidden_units'] = 512 # Number of units in an LSTM cell.
 # config['rnn']['num_hidden_units'] = 512 # GX add
 config['rnn']['dropout_rate'] = 0.3
 # config['rnn']['dropout_rate'] = 0.4
-config['rnn']['num_layers'] = 1 # Number of LSTM stack.
+config['rnn']['num_layers'] = 2 # Number of LSTM stack.
 config['rnn']['num_class_labels'] = 20
 config['rnn']['batch_size'] = config['batch_size']
 config['rnn']['loss_type'] = config['loss_type']
-config['rnn']['regularization_rate'] = 0.0001 # this is the rate for L2 or L1 regularizer
+config['rnn']['regularization_rate'] = 0 # this is the rate for L2 or L1 regularizer
 config['rnn']['moving_average_decay'] = 0.998 # moving_average_rate
 config['rnn']['frame_lenth'] = config['frame_lenth']
 config['rnn']['frame_overlap'] = config['frame_overlap']
